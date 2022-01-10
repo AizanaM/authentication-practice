732ca11ebb90c45fbda52abe41330fd9a32665c3
AikaNiyazova <aikaniyazova@gmail.com>
2022-01-10 10:54:06 +0600

    SecurityConfig modified

--- src/main/java/kg/easyit/authenticationpractice/config/SecurityConfig.java
+++ src/main/java/kg/easyit/authenticationpractice/config/SecurityConfig.java
@@ -6,8 +6,14 @@ import org.springframework.security.config.annotation.web.configuration.WebSecur
 
 @Configuration
 public class SecurityConfig extends WebSecurityConfigurerAdapter {
+
     @Override
     protected void configure(HttpSecurity http) throws Exception {
         super.configure(http);
+    }
+
+    @Override
+    public String toString() {
+        return "SecurityConfig{}";
     }
 }
