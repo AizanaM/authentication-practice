9d708603d0d31bccad4ce57d7aadc18570a5142a
AikaNiyazova <aikaniyazova@gmail.com>
2022-01-10 09:12:19 +0600

    initial commit

---
+++ src/test/java/kg/easyit/authenticationpractice/AuthenticationPracticeApplicationTests.java
@@ -0,0 +1,13 @@
+package kg.easyit.authenticationpractice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AuthenticationPracticeApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
