1b8736020d4ef01e3b8eb6d9e1b2e0c9cc483381
AizanaM <mamakeeva21@gmail.com>
2022-01-10 09:47:56 +0600

    dto+entity

--- src/main/java/kg/easyit/authenticationpractice/model/dto/UserDto.java
+++ src/main/java/kg/easyit/authenticationpractice/model/dto/UserDto.java
@@ -1,4 +1,19 @@
 package kg.easyit.authenticationpractice.model.dto;
 
+
+import lombok.*;
+import lombok.experimental.FieldDefaults;
+
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+@Builder
+@FieldDefaults(level = AccessLevel.PRIVATE)
 public class UserDto {
+   Long id;
+   String firstName;
+   String lastName;
+   String email;
+   String password;
+   String userName;
 }
